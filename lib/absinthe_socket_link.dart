22535ccb7cbc10fe28200526f7cb589ea5111736
Rodolfo Silva <contato@rodolfosilva.com>
2024-04-18 06:57:31 -0400

    Initial commit

---
+++ lib/absinthe_socket_link.dart
@@ -0,0 +1,3 @@
+library absinthe_socket_link;
+
+export 'src/absinthe_socket_link.dart';
---
+++ lib/src/absinthe_socket_link.dart
@@ -0,0 +1,9 @@
+import 'package:graphql/client.dart';
+
+class AbsintheSocketLink extends Link {
+  @override
+  Stream<Response> request(Request request, [NextLink? forward]) {
+    // TODO: implement request
+    throw UnimplementedError();
+  }
+}
